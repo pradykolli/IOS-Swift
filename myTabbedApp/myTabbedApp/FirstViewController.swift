fb37f9770de157663b616fc2a4aae537f3960fec
Pradeep Kolli <pradeepkolli3@yahoo.in>
2019-02-14 10:45:46 -0600

    added two inclass apps

---
+++ myTabbedApp/myTabbedApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  myTabbedApp
+//
+//  Created by Kolli,Pradeep on 2/14/19.
+//  Copyright © 2019 Kolli,Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ myTabbedApp/myTabbedApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  myTabbedApp
+//
+//  Created by Kolli,Pradeep on 2/14/19.
+//  Copyright © 2019 Kolli,Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
